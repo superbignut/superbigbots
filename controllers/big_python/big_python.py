7e81bae843c538b5b0fcfbab5de666e0ff66ca7b
ToryTing <1214002133@qq.com>
2024-09-01 22:28:35 +0800

    first commit

---
+++ controllers/big_python/big_python.py
@@ -0,0 +1,16 @@
+from controller import Robot, Motor, Camera, LED, Keyboard, InertialUnit, Gyro
+import math
+import time
+import spaic
+import torch
+# from manual import manual_robot
+import multiprocessing
+import thread
+import os
+# from intr_emo import ROBOT_NET
+import collections
+import random
+import threading
+import sys
+print(sys.version) # 3.9.13
+print(sys.executable) # pytorch
